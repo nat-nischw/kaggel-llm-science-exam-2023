2d441a1f67fc5ad28b34516eacfed51f120b314c
Natapong Nitarach (Schwyter) <nat.nitarach@gmail.com>
2023-10-09 19:49:52 +0700

    aad: bash.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+pip install git+https://github.com/huggingface/transformers -q
+pip install --upgrade -q wandb
+pip install sentencepiece -q
+pip install -q accelerate -U
+pip install -q transformers[torch]
+pip install datasets colorama -q
