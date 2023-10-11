d5e0f668470304e4059491aebe617f8148663789
Natapong Nitarach (Schwyter) <nat.nitarach@gmail.com>
2023-10-11 14:31:08 +0700

    add: bash fetch_data.py

--- run.sh
+++ run.sh
@@ -1,18 +1,12 @@
 ################## data environment ##################
 mkdir -p data/train-val/
 pip install datasets
-### if convert to csv path:
-### from datasets import load_dataset
-### dataset = load_dataset("natnitaract/kaggel-llm-science-exam-2023-RAG")
-### train_data = dataset['train']
-### train_data.to_csv('data/train-val/train.csv')
-### val_data = dataset['validation']
-### val_data.to_csv('data/train-val/val.csv')
-
+python fetch_data.py
 ### *** you can :: train_data = dataset['train'], val_data = dataset['validation'] *** ###
 
 ################## genprompt environment ##################
 pip install openai itables plotly python-dotenv Wikipedia-API 
+
 ################## tranning environment ##################
 pip install git+https://github.com/huggingface/transformers
 pip install --upgrade wandb
